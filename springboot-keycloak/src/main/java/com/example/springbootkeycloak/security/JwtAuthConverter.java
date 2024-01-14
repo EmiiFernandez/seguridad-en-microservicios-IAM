7987723f11eb7dc4bfd4f41a78943fcdb14aabab
EmiiFernandez <e.fernandezmurgia@gmail.com>
2024-01-14 18:14:57 -0300

    test spring book - keycloak

--- springboot-keycloak/src/main/java/com/example/springbootkeycloak/security/JwtAuthConverter.java
+++ springboot-keycloak/src/main/java/com/example/springbootkeycloak/security/JwtAuthConverter.java
@@ -3,35 +3,86 @@ package com.example.springbootkeycloak.security;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+/*
+ componente de Spring encargado de convertir un objeto JWT (JSON Web Token) en un token de autenticacion de
+ Spring Security (AbstractAuthenticationToken).
+ Este token se utiliza para autenticar y autorizar a los usuarios en la aplicacion.
+ */
 @Component
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
+    // Convertidor de autoridades JWT proporcionado por Spring Security
     private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+    // Propiedades de configuracioon relacionadas con el convertidor de autenticacion JWT
     private final JwtAuthConverterProperties properties;
 
+    // Constructor que recibe las propiedades como parametro
     public JwtAuthConverter(JwtAuthConverterProperties properties) {
         this.properties = properties;
     }
 
+    // Metodo de conversion principal que convierte un JWT en un token de autenticacion de Spring Security
     @Override
     public AbstractAuthenticationToken convert(Jwt jwt) {
+        // Combinar las autoridades del usuario, los roles de recursos y los roles de realm
         Collection<GrantedAuthority> authorities = Stream.concat(
                 jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
-                extractResourceRoles(jwt).stream().collect(Collectors.toSet());
-                return new
-        );
+                Stream.concat(extractResourceRoles(jwt).stream(), extractResourceRolesRealm(jwt).stream())
+        ).collect(Collectors.toSet());
+
+        // Crear y devolver un token de autenticacion de JWT personalizado
+        return new JwtAuthenticationToken(jwt, authorities, getPrincipalClaimName(jwt));
     }
 
-    @Override
-    public <U> Converter<Jwt, U> andThen(Converter<? super AbstractAuthenticationToken, ? extends U> after) {
-        return Converter.super.andThen(after);
+    // Metodo para obtener el nombre del principal (usuario) del JWT
+    // Utiliza la configuracion definida en las propiedades
+    private String getPrincipalClaimName(Jwt jwt) {
+        String claimName = JwtClaimNames.SUB; // Claim predeterminado para el nombre de usuario
+        if (properties.getPrincipalAttribute() != null) {
+            claimName = properties.getPrincipalAttribute();
+        }
+        return jwt.getClaim(claimName);
+    }
+
+    // Metodo para extraer los roles de recursos del JWT
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+        if (resourceAccess == null
+                || (resource = (Map<String, Object>) resourceAccess.get(properties.getResourceId())) == null
+                || (resourceRoles = (Collection<String>) resource.get("roles")) == null) {
+            return Set.of(); // Devolver un conjunto vacio si no hay roles de recursos
+        }
+        return resourceRoles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
+    }
+
+    // Metodo para extraer los roles de realm del JWT
+    private Collection<? extends GrantedAuthority> extractResourceRolesRealm(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim("realm_access");
+        Collection<String> resourceRoles;
+        if (resourceAccess == null
+                || (resourceRoles = (Collection<String>) resourceAccess.get("roles")) == null) {
+            return Set.of(); // Devolver un conjunto vacio si no hay roles de realm
+        }
+        return resourceRoles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
     }
 }
--- springboot-keycloak/src/main/java/com/example/springbootkeycloak/security/JwtAuthConverterProperties.java
+++ springboot-keycloak/src/main/java/com/example/springbootkeycloak/security/JwtAuthConverterProperties.java
@@ -4,27 +4,50 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
 
+/*
+ * Clase de configuracion para el convertidor de autenticacion JWT.
+ * Esta clase actua como un DTO (Objeto de Transferencia de Datos) que carga la configuracion
+ * del convertidor de autenticacion JWT desde las propiedades de la aplicacion.
+ */
 @Validated
 @Configuration
-@ConfigurationProperties(prefix = "jwt.auth.converter") // Toma las config del properties con el prefijo
+@ConfigurationProperties(prefix = "jwt.auth.converter")
 public class JwtAuthConverterProperties {
 
-    //Esta clase será un dto de los datos que trae del jwt.auth.converter
+    // Identificador del recurso JWT
     private String resourceId;
+
+    // Atributo principal utilizado en la autenticacion JWT
     private String principalAttribute;
 
+    /*
+     * Metodo para obtener el identificador del recurso JWT.
+     * @return El identificador del recurso JWT.
+     */
     public String getResourceId() {
         return resourceId;
     }
 
+    /*
+     * Metodo para establecer el identificador del recurso JWT.
+     * @param resourceId El identificador del recurso JWT.
+     */
     public void setResourceId(String resourceId) {
         this.resourceId = resourceId;
     }
 
-    public String getPrincipalAttribute(){
+    /*
+     * Metodo para obtener el atributo principal utilizado en la autenticacion JWT.
+     * @return El nombre del atributo principal.
+     */
+    public String getPrincipalAttribute() {
         return principalAttribute;
     }
 
+    /*
+     * Metodo para establecer el atributo principal utilizado en la autenticacion JWT.
+     * @param principalAttribute El nombre del atributo principal.
+     */
     public void setPrincipalAttribute(String principalAttribute) {
         this.principalAttribute = principalAttribute;
     }
